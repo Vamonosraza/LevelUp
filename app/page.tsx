790c4c5fbf9cfa042af83d3053e38f62ca56669a
Vamonosraza <martinezjessy393@gmail.com>
2024-06-18 21:54:15 -0700

    worked on homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,31 @@
 import Image from "next/image";
 import {Button} from "@/components/ui/button";
-import {Camera} from "lucide-react";
+import jobOffer from "../images/jobOffers.svg"
+import jobCase from "../images/jobCase.svg"
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="h-screen flex items-center justify-center min-h-screen">
-      <Button>Default Button</Button>
-        <Button variant="outline" size={"icon"}>
-            <Camera/>
-        </Button>
-      <h1>Home</h1>
-    </div>
+    <main>
+        <header className="max-w-6xl mx-auto px-4 sm:px-8 py-6">
+            <Image src={jobCase} alt={"logo"} className={"w-24 h-16"} />
+        </header>
+        <section className="max-w-6xl mx-auto px-4 sm:px-8 h-screen -mt-20 grid lg:grid-cols-[1fr,400px] items-center">
+            <div>
+                <h1 className="capitalize text-4xl font-bold text-gray-900 mid:text-7xl">
+                    Job <span className={"text-primary"}>tracking</span> app
+                </h1>
+                <p className="leading-loose max-w-md mt-4">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur dignissimos dolorum, explicabo iusto libero minima molestiae odio optio provident qui, ratione suscipit veniam. Eos fugiat illo libero nobis unde voluptate.
+                </p>
+                <Button asChild className={"mt-4"}>
+                    <Link href={'/add-job'}>
+                        Get Started
+                    </Link>
+                </Button>
+            </div>
+            <Image src={jobOffer} alt={'landing'} className="hidden lg:block" />
+        </section>
+    </main>
   );
 }
