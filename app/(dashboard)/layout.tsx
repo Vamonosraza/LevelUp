56cf0adc06197637af2691ad307f7083c691a9d5
Vamonosraza <martinezjessy393@gmail.com>
2024-06-23 20:49:19 -0700

    Working on navbar and links

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import React, { PropsWithChildren } from 'react'
+import Navbar from '@/components/Navbar'
+import Sidebar from '@/components/Sidebar'
 
-const layout = ({children}: {children:React.ReactNode}) => {
+const layout = ({children}: PropsWithChildren) => {
   return (
-    <div>{children}</div>
+    <main className='grid lg:grid-cols-5'>
+      <div className='hidden lg:block lg:col-span-1 lg:min-h-screen'>
+        <Sidebar />
+      </div>
+      <div className='lg:col-span-4'>
+        <Navbar />
+        <div className='py-16 px-4 sm:px-8 lg:px-16'>
+
+          {children}
+        </div>
+      </div>
+    </main>
   )
 }
 
