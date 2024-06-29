05a63254b025710a33b401ea727a081407d85053
Vamonosraza <martinezjessy393@gmail.com>
2024-06-29 12:36:50 -0700

    Was getting errors so I went back

--- components/FormComponents.tsx
+++ components/FormComponents.tsx
@@ -1,68 +1,84 @@
-import {Control, Form} from 'react-hook-form';
-import { Select, SelectContent, SelectItem, SelectTrigger,SelectValue } from '@radix-ui/react-select';
-import { FormField, FormControl, FormItem, FormMessage, FormLabel } from './ui/form';
-import { Input } from '@/components/ui/input';
+import { Control } from 'react-hook-form';
+import {
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+    } from '@/components/ui/select';
+    import {
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+    } from '@/components/ui/form';
+    import { Input } from './ui/input';
 
-type CustomFormFieldProps = {
-
+    type CustomFormFieldProps = {
     name: string;
     control: Control<any>;
-};
+    };
 
-export function CustomFormField({name, control}:CustomFormFieldProps) {
-    return(
+    export function CustomFormField({ name, control }: CustomFormFieldProps) {
+    return (
         <FormField
         control={control}
         name={name}
-        render={({field})=>(
+        render={({ field }) => (
             <FormItem>
-                <FormLabel className='capitalize'>{name}</FormLabel>
-                <FormControl>
-                    <FormLabel htmlFor={name}>{name}</FormLabel>
-                    <Input {...field} id={name} />
-                </FormControl>
-                <FormMessage />
+            <FormLabel className='capitalize'>{name}</FormLabel>
+            <FormControl>
+                <Input {...field} />
+            </FormControl>
+            <FormMessage />
             </FormItem>
         )}
-        />)
-}
+        />
+    );
+    }
 
-type CustomFormSelectProps = {
+    type CustomFormSelectProps = {
     name: string;
     control: Control<any>;
     items: string[];
     labelText?: string;
-};
+    };
 
-export function CustomFormSelect({name, control, items, labelText}:CustomFormSelectProps) {
+    export function CustomFormSelect({
+    name,
+    control,
+    items,
+    labelText,
+    }: CustomFormSelectProps) {
     return (
         <FormField
-            control={control}
-            name={name}
-            render={({ field }) => (
-                <FormItem>
-                <FormLabel className='capitalize'>{labelText || name}</FormLabel>
-                <Select onValueChange={field.onChange} defaultValue={field.value}>
-                    <FormControl>
-                    <SelectTrigger>
-                        <SelectValue />
-                    </SelectTrigger>
-                    </FormControl>
-                    <SelectContent>
-                    {items.map((item) => {
-                        return (
-                        <SelectItem key={item} value={item}>
-                            {item}
-                        </SelectItem>
-                        );
-                    })}
-                    </SelectContent>
-                </Select>
-        
-                <FormMessage />
-                </FormItem>
-            )}
-            />
-        );
-    
+        control={control}
+        name={name}
+        render={({ field }) => (
+            <FormItem>
+            <FormLabel className='capitalize'>{labelText || name}</FormLabel>
+            <Select onValueChange={field.onChange} defaultValue={field.value}>
+                <FormControl>
+                <SelectTrigger>
+                    <SelectValue />
+                </SelectTrigger>
+                </FormControl>
+                <SelectContent>
+                {items.map((item) => {
+                    return (
+                    <SelectItem key={item} value={item}>
+                        {item}
+                    </SelectItem>
+                    );
+                })}
+                </SelectContent>
+            </Select>
+
+            <FormMessage />
+            </FormItem>
+        )}
+        />
+    );
 }
+export default CustomFormSelect;
