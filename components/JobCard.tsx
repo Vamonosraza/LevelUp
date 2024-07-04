8830c679ff044a4fb606fe718759b8168aa798bd
Vamonosraza <martinezjessy393@gmail.com>
2024-07-04 15:33:16 -0700

    Added position and company description to jobcard as well as buttons for editing and delete, which I will work on more later.

--- components/JobCard.tsx
+++ components/JobCard.tsx
@@ -1,9 +1,35 @@
 import { JobType } from '@/utils/types'
 import React from 'react'
+import { MapPin, Briefcase, CalendarDays, RadioTower } from 'lucide-react'
+import Link from 'next/link'
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card'
+import { Separator } from './ui/separator'
+import { Button } from '@/components/ui/button'
+import { Badge } from '@/components/ui/badge'
+import JobInfo from './JobInfo'
+import DeleteJobBtn from './DeleteJobBtn'
 
-const JobCard = ({job}:{job:JobType}) => {
+
+function JobCard  ({job}:{job:JobType}) {
   return (
-    <div className='text-4xl'>JobCart</div>
+    <Card className='bg-muted'>
+      <CardHeader>
+        <CardTitle>{job.position}</CardTitle>
+        <CardDescription>{job.company}</CardDescription>
+      </CardHeader>
+      <Separator />
+      <CardContent>
+
+      </CardContent>
+      <CardFooter className='flex gap-4'>
+      <Button asChild size={'sm'}>
+        <Link href={`/jobs/${job.id}`}>
+          View
+        </Link>
+      </Button>
+      <DeleteJobBtn/>
+      </CardFooter>
+    </Card>
   )
 }
 
