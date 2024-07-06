e29e503481e8cb73e1e038b3fb840065f27a9d3c
Vamonosraza <martinezjessy393@gmail.com>
2024-07-06 15:13:58 -0700

    feat: Add loading component for stats page and now getting error for chart container

--- components/ChartsContainer.tsx
+++ components/ChartsContainer.tsx
@@ -1,8 +1,33 @@
 import React from 'react'
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts'
+import { useQuery } from '@tanstack/react-query'
+import { getChartsDataAction } from '@/utils/actions'
+
 
 function ChartsContainer () {
+    const { data, isPending} = useQuery({
+        queryKey:['charts'],
+        queryFn:() => getChartsDataAction()
+    })
+
+    if(isPending) return <h2 className='text-xl'>Loading...</h2>
+    if(!data || data.length <1) return null
     return (
-        <h2 className='text-xl'>ChartsContainer</h2>
+        <section className='mt-16'>
+            <h1 className='text-4xl font-semibold text-center'>
+                Monthly Applications
+            </h1>
+            <ResponsiveContainer width='100%' height={300}>
+                <BarChart
+                    data={data} margin={{top:5}} >
+                    <CartesianGrid strokeDasharray='3 3' />
+                    <XAxis dataKey='date' />
+                    <YAxis allowDecimals={false}/>
+                    <Tooltip />
+                    <Bar dataKey='count' fill='#8884d8' barSize={75}/>
+                </BarChart>
+            </ResponsiveContainer>
+        </section>
     )
 }
 
